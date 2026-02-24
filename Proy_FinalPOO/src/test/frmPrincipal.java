ca3f8ea70b14b0f24e4f1a651a13587c36b03e55
User <User@DESKTOP-HBE52SB>
2026-02-23 20:16:31 -0500

    TUVE UN ERROR Y TUVE QUE CMABIAR ALGO

--- Proy_FinalPOO/src/test/frmPrincipal.java
+++ Proy_FinalPOO/src/test/frmPrincipal.java
@@ -15,6 +15,7 @@ import gestion.*;
 public class frmPrincipal extends javax.swing.JFrame {
     private GestionPartido gestorPartido;
     private GestionCandidato gestorCandidato;
+    
 
     /**
      * Creates new form frmPrincipal
@@ -48,6 +49,7 @@ public class frmPrincipal extends javax.swing.JFrame {
         jLabel1 = new javax.swing.JLabel();
         PartidosPoliticos = new javax.swing.JButton();
         ActasElectorales = new javax.swing.JButton();
+        jButton1 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
@@ -97,6 +99,13 @@ public class frmPrincipal extends javax.swing.JFrame {
             }
         });
 
+        jButton1.setText("Reporte General");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout bgLayout = new javax.swing.GroupLayout(bg);
         bg.setLayout(bgLayout);
         bgLayout.setHorizontalGroup(
@@ -106,6 +115,7 @@ public class frmPrincipal extends javax.swing.JFrame {
                 .addGroup(bgLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(jLabel1)
                     .addGroup(bgLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                        .addComponent(jButton1, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                         .addComponent(ActasElectorales, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                         .addComponent(PartidosPoliticos, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                         .addComponent(Elecciones, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
@@ -126,7 +136,9 @@ public class frmPrincipal extends javax.swing.JFrame {
                 .addComponent(PartidosPoliticos)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(ActasElectorales)
-                .addContainerGap(345, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jButton1)
+                .addContainerGap(306, Short.MAX_VALUE))
             .addComponent(content, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 510, Short.MAX_VALUE)
         );
 
@@ -162,6 +174,11 @@ public class frmPrincipal extends javax.swing.JFrame {
         ActasElectorales a= new ActasElectorales();
         mostrarPanel(a);
     }//GEN-LAST:event_ActasElectoralesActionPerformed
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        ReportedeCandidato rep = new ReportedeCandidato(gestorCandidato);
+        mostrarPanel(rep);
+    }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -225,6 +242,7 @@ public class frmPrincipal extends javax.swing.JFrame {
     private javax.swing.JButton PartidosPoliticos;
     private javax.swing.JPanel bg;
     private javax.swing.JPanel content;
+    private javax.swing.JButton jButton1;
     private javax.swing.JLabel jLabel1;
     // End of variables declaration//GEN-END:variables
 }
